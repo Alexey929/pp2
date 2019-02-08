37f45b09aa70897cd5e1dd3f0e362f334ba36a67
Alexey929 <thelom05@mail.ru>
2019-02-08 13:01:49 +0600

    Labs

---
+++ Task 3/Task 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for ( int i =0; i<n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            int[] b = new int[n * 2];
+            int k = -1;
+            for ( int i = 0; i < n * 2; i++)
+            {
+                if ( i%2 == 0)
+                {
+                    k++;
+                }
+                b[i] = a[k];
+            }
+            for (int i = 0; i < n * 2; i++)
+            {
+                Console.Write(b[i]);
+                Console.Write(' ');
+            }
+
+            Console.WriteLine("\n Для продолжения нажмите любую клавишу...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool f(int b)
+            {
+                if (b == 1)
+                {
+                    return false;
+                }
+                int k = 0;
+                for (int i = 2; i < b; i++)
+                {
+                    if (b % i == 0) { k++; }
+                }
+                if (k == 0)
+                {
+                    return true;
+                }
+
+                else return false;
+            }
+            int cnt = 0;
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for ( int i=0; i < n; i++)
+            {
+               a[i] = int.Parse(Console.ReadLine());
+            }
+            for ( int i = 0; i <n; i++)
+            {
+                if ( f(a[i])== true)
+                {
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+            for ( int i = 0; i < n; i++)
+            {
+                if ( f(a[i]) == true)
+                {
+                    Console.Write(a[i]);
+                    Console.Write(' ');
+                }
+            }
+
+            
+            Console.WriteLine("\n Для продолжения нажмите любую клавишу...");
+            Console.ReadKey(true);
+
+        }
+    }
+}
