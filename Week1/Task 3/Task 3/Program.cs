834e9effc046821715d632f3ffcf1505ba366e98
Alexey929 <thelom05@mail.ru>
2019-02-12 13:16:37 +0600

    abc

--- Week1/Task 3/Task 3/Program.cs
+++ Week1/Task 3/Task 3/Program.cs
@@ -10,29 +10,34 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] a = new int[n];
-            for ( int i =0; i<n; i++)
+            int n = int.Parse(Console.ReadLine()); // вводим кол-во чисел, он же размер массива
+            int[] a = new int[n];    // создаем массив чисел
+
+            string s = Console.ReadLine();  // считываем числа в строку
+            string[] str = new string[n];   // создаем массив строк
+            str = s.Split();     // записываем разделенные пробелом числа в массив из строк str
+
+            for ( int i =0; i<n; i++)    // цикл по элементам массива
             {
-                a[i] = int.Parse(Console.ReadLine());
+                a[i] = int.Parse(str[i]);  // преобразовываем методом Parse числа из массива строк в массив чисел
             }
-            int[] b = new int[n * 2];
-            int k = -1;
-            for ( int i = 0; i < n * 2; i++)
+            int[] b = new int[n * 2]; // создаем новый массив чисел размером в два раза больше массива а[]
+            int k = -1;        // коэффициент k
+            for ( int i = 0; i < n * 2; i++)    // цикл по массиву
             {
-                if ( i%2 == 0)
+                if ( i%2 == 0)         // сдвигаем индекс элемента массива a[] только когда выбираем элемент из массива b  с четной позицией
                 {
                     k++;
                 }
-                b[i] = a[k];
+                b[i] = a[k];      // заполняем массив b[]
             }
-            for (int i = 0; i < n * 2; i++)
+            for (int i = 0; i < n * 2; i++)   // цикл по массиву b 
             {
-                Console.Write(b[i]);
+                Console.Write(b[i]);       // выводим элементы массива b через запятую
                 Console.Write(' ');
             }
 
-            Console.WriteLine("\n Для продолжения нажмите любую клавишу...");
+            Console.WriteLine("\n Для продолжения нажмите любую клавишу..."); // чтобы не закрывалось сразу при отладке
             Console.ReadKey(true);
         }
     }
