431566fefc6d60e610c684f65b1782ae4d1954da
Alexey929 <thelom05@mail.ru>
2019-02-14 22:08:16 +0600

    lab

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Create(@"C:\Users\123\source\repos\Week2\path\TextFile.txt");
+            var TextFile = File.Create(@"C:\Users\123\source\repos\Week2\path\TextFile.txt");
+            TextFile.Close();
+            File.Copy(@"C:\Users\123\source\repos\Week2\path\TextFile.txt", @"C:\Users\123\source\repos\Week2\path1\TextFile.txt");
+            File.Delete(@"C:\Users\123\source\repos\Week2\path\TextFile.txt");
+            Console.WriteLine("Для продолжения нажмите любую клавишу...");
+            Console.ReadKey(true);
+        }
+    }
+}
